9e0f667d8ed0db89631350698ed52d3aa4664fb7
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-06 23:14:45 +0330

    create Navbar

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,10 @@
+import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
