522a5688a24926c07aa7325eb424cf0850c4b23b
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-06 23:44:42 +0330

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find book, or rent a car - quickly and easily!
+          Find book, or rent a car -- quickly and easily!
         </h1>
 
         <p className="hero__subtitle">
@@ -27,9 +27,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-        
-          <div className="hero__image-overlay"></div>
         </div>
+          <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
