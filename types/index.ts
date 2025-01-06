c4888745b75e752ef8d71db5ffa7ba5e11966a72
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-06 20:43:08 +0330

    create custom button

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: String;
+  containerStyle?: String;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
