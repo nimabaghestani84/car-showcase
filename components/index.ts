d746e657068a9dcf16d38ff28caaec46d409a06d
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-07 22:40:12 +0330

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,16 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchMenufacturer from "./SearchMenufacturer";
+
 export{
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchMenufacturer
 }
