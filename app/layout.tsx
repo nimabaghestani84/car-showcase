a49ebe1d16e9fa66ce21922f1fe848c98ceaa7bc
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-26 13:46:00 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,19 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import { Footer } from "@/components";
-import Navbar from "@/components/Navbar";
 
-export const metadata: Metadata = {
+import { Footer, NavBar } from "@components";
+
+export const metadata = {
   title: "Car Hub",
-  description: "Discover the best cars in the world.",
+  description: "Discover world's best car showcase application",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`relative`}
-      >
-        <Navbar/>
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
