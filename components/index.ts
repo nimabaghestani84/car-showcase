b8f0f8a4750b9a0983ca2c88a8186a97db65226b
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-01-27 10:13:04 +0330

    end

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,19 @@
-import CustomButton from "./CustomButton";
-import Hero from "./Hero";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import SearchBar from "./SearchBar";
-import CustomFilter from "./CustomFilter";
-import SearchMenufacturer from "./SearchMenufacturer";
 import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
 
-export{
-    Hero,
-    CustomButton,
-    Navbar,
-    Footer,
-    SearchBar,
-    CustomFilter,
-    SearchMenufacturer,
-    CarCard,
-}
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
